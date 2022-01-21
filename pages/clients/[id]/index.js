ad29a71586afadb4965374cd35f6ef420a1b11d8
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-21 16:34:51 +0700

    navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,14 @@ const ClientProjectsPage = () => {
   const router = useRouter();
 
   console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.push(`/clients/${router.query.id}/projecta`);
+  };
   return (
     <div>
       <h1>The Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
